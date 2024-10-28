6ff9a7915417d00451c4b715a597e2927a7a9eda
camdelay16 <camdelay16@gmail.com>
2024-10-28 15:04:00 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+// src/App.jsx
+const App = () => {
+  const todo = { text: 'A brand new task', done: true };
+  const todos = [
+    {text: 'Learn JavaScript', done: true},
+    {text: 'Learn JSX', done: false},
+    {text: 'Learn HTML', done: true},
+    {text: 'Learn CSS', done: true},
+    {text: 'Master React', done: false},
+  ];
+
+  const todoList = todos.map((loopedTodo, index) => 
+    <li key={index}>
+      {loopedTodo.done ? `Task Completed - ${loopedTodo.text}` : loopedTodo.text }
+    </li>
+  )
+  
+
+  return (
+    <>
+      <h1 className="container">JavaScript in JSX</h1>
+      <p>{todo.text}</p>
+
+      <h2>Conditional Rendering</h2>
+      <p>{ todo.done ? `Task Completed - ${todo.text}` : todo.text }</p>
+
+      <h2>Looping with JSX</h2>
+      <ul>{todoList}</ul>
+    </>
+  )
+}
+
+export default App;
